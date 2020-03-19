47243a2f0579e12f28cad6985fdeef22ebafe859
pmcheng <pmcheng99@gmail.com>
2020-03-19 15:17:54 +0800

    delete everything

--- express-app/public/javascripts/decoder.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //解密base64
-    decodeBase64 : function(encode){
-        let buff = new Buffer.from(encode, 'base64');
-        let text = buff.toString();
-        return text;
-    }
-};
